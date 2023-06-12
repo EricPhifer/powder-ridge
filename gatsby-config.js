9961f4045be6f2283d6d7e5c12e5a7e3b55d2f83
Eric Phifer <ericoncode@gmail.com>
2023-06-12 13:04:19 -0600

    add gtag details for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,33 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-google-gtag',
     'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-LSBFGY5478', // Google Analytics / GA
+          // AdWords & Google Ads ID
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional [sets Respect Do Not Track]
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
